d72507df2354b3583dff623cffd32859f80f1dc4
itz-amethyst <pransermi@gmail.com>
2023-06-23 19:57:47 +0430

    feat: Added Redis Bootstrapper

---
+++ RedisApiManagement.Infrastructure.Configuration/RedisBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace RedisApiManagement.Infrastructure.Configuration
+{
+    public class RedisBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IConnectionMultiplexer>(option =>
+                ConnectionMultiplexer.Connect(connectionString));
+        }
+    }
+}
