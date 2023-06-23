04528a7855c414d02a65d279dae88c3a5284ebaa
itz-amethyst <pransermi@gmail.com>
2023-06-23 19:58:56 +0430

    cleanup

--- RedisApi/Program.cs
+++ RedisApi/Program.cs
@@ -1,6 +1,14 @@
+using RedisApiManagement.Infrastructure.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//? Redis Configuration
+
+var connectionString = builder.Configuration.GetConnectionString("RedisConnection");
+
+RedisBootstrapper.Configure(builder.Services , connectionString);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
