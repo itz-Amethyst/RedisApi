057fe68a0d395bca6ec86cb873075840aa0ca479
itz-amethyst <pransermi@gmail.com>
2023-06-23 20:10:37 +0430

    feat: Added Methods to IPlatformRepository

--- RedisApiManagement.Domain/Platform/IPlatformRepository.cs
+++ RedisApiManagement.Domain/Platform/IPlatformRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace RedisApiManagement.Domain.Platform
+﻿using RedisApiManagement.Application.Contracts.Platform;
+
+namespace RedisApiManagement.Domain.Platform
 {
     public interface IPlatformRepository
     {
+        // Can Create A base generic repository for common methods but because its small not necessary
+
+        void CreatePlatform(CreatePlatform command);
+
+        PlatformViewModel? GetPlatformById(string platformId);
+
+        IEnumerable<PlatformViewModel> GetAllPlatforms();
     }
 }
