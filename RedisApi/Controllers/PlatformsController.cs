8dd87ff82a1258e248cbe170af10085825e2de65
itz-amethyst <pransermi@gmail.com>
2023-06-23 20:15:25 +0430

    feat: Added PlatformsController

---
+++ RedisApi/Controllers/PlatformsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisApiManagement.Application.Contracts.Platform;
+using RedisApiManagement.Domain.Platform;
+
+namespace RedisApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepository _platformRepository;
+
+        public PlatformsController(IPlatformRepository platformRepository)
+        {
+            _platformRepository = platformRepository;
+        }
+
+        [HttpGet("{id}" , Name = "GetPlatformById")]
+        public ActionResult<PlatformViewModel> GetPlatformById(string id)
+        {
+            var platform = _platformRepository.GetPlatformById(id);
+
+            if (platform != null)
+            {
+                return Ok(platform);
+            }
+
+            return NotFound();
+        }
+    }
+}
